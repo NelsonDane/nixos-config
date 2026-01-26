a1489c298d127539da722ca3e85ed6db0b83c13c
Nelson Dane <47427072+NelsonDane@users.noreply.github.com>
2026-01-26 15:58:56 -0500

    gpg only desktop

--- users/ndane/desktop.nix
+++ users/ndane/desktop.nix
@@ -1,5 +1,12 @@
 { pkgs, lib, ... }:
 
 lib.mkIf pkgs.stdenv.isLinux {
-  home.packages = with pkgs; [];
+  home.packages = with pkgs; [
+    pinentry-curses # for gpg
+  ];
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    pinetryFormat = "curses";
+  };
 }
