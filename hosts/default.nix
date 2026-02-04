6b5608f2b214db3c3bc01ea023ae571a3a8d5d11
Nelson Dane <47427072+NelsonDane@users.noreply.github.com>
2026-02-04 17:56:22 -0500

    enable zsh default
    Signed-off-by: Nelson Dane <47427072+NelsonDane@users.noreply.github.com>

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,6 +1,22 @@
-{ config, pkgs, ... }:
-
+{ pkgs, ... }:
 {
+  # Nix settings
   nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+  nix.settings.trusted-users = [ "ndane" ];
+
+  # Set default shell to zsh
+  programs.zsh.enable = true;
+  users.users.ndane.shell = pkgs.zsh;
+
+  # Direnv configuration (temp environments)
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+    silent = true;
+    enableZshIntegration = true;
+  };
 }
