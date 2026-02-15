c5bec6e00a721431e274dca065e3449ec6f49bd8
Nelson Dane <47427072+NelsonDane@users.noreply.github.com>
2026-02-15 10:31:20 -0500

    mac gpg
    Signed-off-by: Nelson Dane <47427072+NelsonDane@users.noreply.github.com>

--- users/ndane/darwin.nix
+++ users/ndane/darwin.nix
@@ -1,4 +1,11 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [ ];
+  home.packages = with pkgs; [ pinentry_mac ];
+
+  # GPG configuration
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+    pinentry.package = pkgs.pinentry_mac;
+  };
 }
