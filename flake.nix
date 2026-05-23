8e043140ce5dc7c73bb324c7b630b6d1fa01b769
Nelson Dane <47427072+NelsonDane@users.noreply.github.com>
2026-05-23 17:20:18 -0400

    small fix
    Signed-off-by: Nelson Dane <47427072+NelsonDane@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -14,10 +14,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nix-homebrew = {
-      url = "github:zhaofengli/nix-homebrew";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
 
     disko = {
       url = "github:nix-community/disko";
