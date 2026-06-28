4cabdb423db5ef841c9cf75febc19a1d07987be3
Nelson Dane <47427072+NelsonDane@users.noreply.github.com>
2026-06-27 23:18:20 -0400

    fix sddm monitor layout
    Signed-off-by: Nelson Dane <47427072+NelsonDane@users.noreply.github.com>

--- hosts/desktop/sddm.nix
+++ hosts/desktop/sddm.nix
@@ -1,13 +1,38 @@
-{ pkgs, ... }: {
+{ pkgs, config, lib, ... }:
+let
+  westonIni = pkgs.writeText "weston-sddm.ini" ''
+    [libinput]
+    enable-tap=${lib.boolToString config.services.libinput.mouse.tapping}
+    left-handed=${lib.boolToString config.services.libinput.mouse.leftHanded}
+
+    [keyboard]
+    keymap_model=${config.services.xserver.xkb.model}
+    keymap_layout=${config.services.xserver.xkb.layout}
+    keymap_variant=${config.services.xserver.xkb.variant}
+    keymap_options=${config.services.xserver.xkb.options}
+
+    [output]
+    name=HDMI-A-1
+    mode=off
+
+    [output]
+    name=DP-1
+    mode=off
+
+    [output]
+    name=DP-2
+    mode=preferred
+  '';
+in {
   services.displayManager.sddm = {
     enable = true;
-    wayland.enable = true;
+    wayland = {
+      enable = true;
+      compositorCommand = "${pkgs.weston}/bin/weston --shell=kiosk -c ${westonIni}";
+    };
     package = pkgs.kdePackages.sddm;
     theme = "sddm-astronaut-theme";
   };
-  services.xserver.displayManager.setupCommands = ''
-    /run/current-system/sw/bin/xrandr --output DP-2 --mode 2560x1440 --rate 144 --primary
-  '';
 
   environment.systemPackages = with pkgs; [
     (sddm-astronaut.override { embeddedTheme = "black_hole"; })
