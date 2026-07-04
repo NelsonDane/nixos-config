3b3836e105c2eea6be483f55e5831036a881930b
Nelson Dane <47427072+NelsonDane@users.noreply.github.com>
2026-07-04 13:05:31 -0400

    zfs snapshots
    Signed-off-by: Nelson Dane <47427072+NelsonDane@users.noreply.github.com>

--- hosts/nas/zfs.nix
+++ hosts/nas/zfs.nix
@@ -1,6 +1,10 @@
 _: {
+  # https://nixos.wiki/wiki/ZFS
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.extraPools = [ "Shell" ];
-  services.zfs.autoScrub.enable = true;
-  services.zfs.trim.enable = true;
+  services.zfs = {
+    autoScrub.enable = true;
+    autoSnapshot.enable = true;
+    trim.enable = true;
+  };
 }
--- justfile
+++ justfile
@@ -18,7 +18,7 @@ desktop:
 
 nas:
   # just switch nas
-  NIX_SSHOPTS="-i ~/.ssh/nas" nh os switch .#nas --target-host ndane@10.0.2.20 --build-host ndane@10.0.2.15
+  NIX_SSHOPTS="-i ~/.ssh/nas" nh os switch .#nas --target-host ndane@10.0.2.20 --build-host ndane@10.0.2.20
 
 macbook:
   nh darwin switch .#macbook
