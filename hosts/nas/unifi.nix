927e9540af38f1747a7dd45a02a5854222b80866
Nelson Dane <47427072+NelsonDane@users.noreply.github.com>
2026-07-12 19:25:01 -0400

    fix mongodb
    Signed-off-by: Nelson Dane <47427072+NelsonDane@users.noreply.github.com>

--- hosts/nas/unifi.nix
+++ hosts/nas/unifi.nix
@@ -1,7 +1,9 @@
-_: {
+{ pkgs, ... }: {
   # https://mynixos.com/nixpkgs/options/services.unifi
   services.unifi = {
     enable = true;
     openFirewall = true;
+    # https://github.com/NixOS/nixpkgs/issues/461961
+    mongodbPackage = pkgs.mongodb-ce;
   };
 }
