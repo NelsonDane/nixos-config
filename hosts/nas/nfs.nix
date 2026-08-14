27c0627b900552b578d2b1f55bbb1efe365b0faf
Nelson Dane <47427072+NelsonDane@users.noreply.github.com>
2026-08-14 15:04:10 -0400

    fix nfs
    Signed-off-by: Nelson Dane <47427072+NelsonDane@users.noreply.github.com>

--- hosts/nas/nfs.nix
+++ hosts/nas/nfs.nix
@@ -4,7 +4,11 @@ _: {
     exports = ''
       /Shell/media        10.0.2.0/24(rw,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
       /Shell/ISOs         10.0.2.5(rw,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
-      /Shell/kube-backups 10.0.2.50(rw,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
+      /Shell/kube-backup 10.0.2.50(rw,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
+      /Shell/cloud        10.0.2.51(rw,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
+      /Shell/cloud        10.0.2.52(rw,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
+      /Shell/cloud        10.0.2.53(rw,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
+      /Shell/cloud        10.0.2.54(rw,all_squash,anonuid=1000,anongid=1000,no_subtree_check)
     '';
   };
   networking.firewall.allowedTCPPorts = [ 2049 ];
